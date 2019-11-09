70db3c39e232f497e8e7263ae96a401042b84a8e
milletGuo <zzujinxi@126.com>
2019-11-09 16:50:48 +0800

    在给表单提供value时，应同时提供onChange方法，因此删除value属性

--- src/Component/EditData.jsx
+++ src/Component/EditData.jsx
@@ -12,22 +12,22 @@ class EditData extends React.Component {
                 <form className="layer-content">
                     <div>
                         <label>姓名：</label>
-                        <input type="text" name="name" placeholder="请输入姓名" autoComplete="off" value="" />
+                        <input type="text" name="name" placeholder="请输入姓名" autoComplete="off" />
                     </div>
                     <div>
                         <label>性别：</label>
-                        <select name="sex" value={"男"}  >
+                        <select name="sex"  >
                             <option value="男">男</option>
                             <option value="女">女</option>
                         </select>
                     </div>
                     <div>
                         <label>年龄：</label>
-                        <input type="text" name="age" placeholder="请输入年龄" autoComplete="off" value="" />
+                        <input type="text" name="age" placeholder="请输入年龄" autoComplete="off" />
                     </div>
                     <div>
                         <label>角色：</label>
-                        <select name="role" value={"教师"} >
+                        <select name="role" >
                             <option value="教师">教师</option>
                             <option value="学生">学生</option>
                         </select>
